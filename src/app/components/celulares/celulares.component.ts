c1c583fa915c11d2329dc79da4b42b19da25d4d2
Danielle Amaral Constâncio e Silva <danielle.constancio@alliedbrasil.com.br>
2021-09-27 17:42:30 -0300

    add ordenação nos parametros

--- src/app/components/celulares/celulares.component.ts
+++ src/app/components/celulares/celulares.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  Router,
+} from '@angular/router';
+import { URLConfig } from 'src/app/resolvers/teste.resolver';
 
 @Component({
   selector: 'app-celulares',
@@ -13,5 +18,57 @@ export class CelularesComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.activatedRoute);
+  }
+
+  getUrl(keyParam: string): string {
+    const urlParams = this.activatedRoute.snapshot.children[0].url;
+    let url = '';
+    const params: URLConfig[] = [];
+    for (let i = 0; i < urlParams.length; i++) {
+      const key = urlParams[i]?.path;
+      const value = urlParams[i + 1]?.path;
+      const order = this.getOrderParamByKey(key);
+      const urlConfig: URLConfig = { key, value, order };
+
+      params.push(urlConfig);
+      i++;
+    }
+
+    const paramsOrdered = params.sort((a, b) => {
+      return a.order - b.order;
+    });
+    for (const param of paramsOrdered) {
+      url += `/${param.key}/${param.value}`;
+    }
+
+    console.log(url);
+    return url;
+  }
+
+  getOrderParamByKey(key: string): number {
+    if (key === 'marca') {
+      return 0;
+    }
+
+    if (key === '') {
+      return 1;
+    }
+
+    if (key === 'pagina') {
+      return 2;
+    }
+
+    if (key === 'plano') {
+      return 3;
+    }
+
+    if (key === 'preco') {
+      return 4;
+    }
+    if (key === 'preco-ate') {
+      return 5;
+    }
+
+    return 0;
   }
 }
--- src/app/resolvers/teste.resolver.ts
+++ src/app/resolvers/teste.resolver.ts
@@ -2,22 +2,62 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
+export interface URLConfig {
+  key: string;
+  value: string;
+  order: number;
+}
+
 @Injectable({ providedIn: 'root' })
 export class TesteResolver implements Resolve<string> {
   resolve(
     route: ActivatedRouteSnapshot
   ): Observable<string> | Promise<string> | string {
-    const params = [];
+    const params: URLConfig[] = [];
     for (let i = 0; i < route.url.length; i++) {
-      const key = route.url[i].path;
-      const value = route.url[i + 1].path;
+      const key = route.url[i]?.path;
+      const value = route.url[i + 1]?.path;
+      const order = this.getOrderParamByKey(key);
+      const urlConfig: URLConfig = { key, value, order };
 
-      params.push({ key, value });
+      params.push(urlConfig);
       i++;
     }
 
     console.log(params);
 
+    const paramsOrdered = params.sort((a, b) => {
+      return a.order - b.order;
+    });
+    console.log(paramsOrdered);
+
     return 'teste';
+  }
+
+  getOrderParamByKey(key: string): number {
+    if (key === 'marca') {
+      return 0;
+    }
+
+    if (key === '') {
+      return 1;
+    }
+
+    if (key === 'pagina') {
+      return 2;
+    }
+
+    if (key === 'plano') {
+      return 3;
+    }
+
+    if (key === 'preco') {
+      return 4;
+    }
+    if (key === 'preco-ate') {
+      return 5;
+    }
+
+    return 0;
   }
 }
